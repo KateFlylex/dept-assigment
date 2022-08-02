1d546baa5aec3b475956a2d1bc76d95dbdd693d8
Kateryna Kurnus <kkurnus@deloitte.com>
2022-08-02 14:56:33 +0200

    fix Quote component styles

--- src/components/Quote/Quote.tsx
+++ src/components/Quote/Quote.tsx
@@ -3,11 +3,11 @@ import React from 'react';
 const Quote = () => {
 
   return (
-    <div className="p-20 text-white w-full">
-      <div className="text-3xl font-extralight m-auto w-fit text-center">
+    <div className="p-12 md:p-20 text-white w-full">
+      <div className="text-2xl md:text-3xl font-extralight m-auto w-fit text-center">
         "Dept helped us tell our story through a bold new identity and a robust online experience. To the tone of 60% growth in online bookings in just one month"
       </div>
-      <div className="text-lg uppercase font-extralight m-auto w-fit pt-8">
+      <div className="md:text-lg uppercase font-extralight m-auto w-fit pt-8 text-center">
         MATTIJS TEN DRINK - CEO, TRANSAVIA
       </div>
     </div>
